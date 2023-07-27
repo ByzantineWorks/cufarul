894aa5b472b643f9bfdf8b27ec9d345e529273ea
Alexandru N. Onea <onea.alex@gmail.com>
2023-07-27 15:36:43 +0300

    fields: add initial tests

--- src/fields.rs
+++ src/fields.rs
@@ -136,3 +136,31 @@ impl Field<NonEmptyString> for TranslatableField {
 		Ok(value.unwrap())
 	}
 }
+
+/* Testing */
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_non_empty_string () {
+        assert!(NonEmptyString::try_from(String::from("")).is_err());
+    }
+
+    #[test]
+    fn test_lang_string_serde () -> Result<()> {
+        assert_eq!(String::from(Lang::AR), String::from("ar"));
+        assert_eq!(String::from(Lang::EN), String::from("en"));
+        assert_eq!(String::from(Lang::GR), String::from("gr"));
+        assert_eq!(String::from(Lang::RO), String::from("ro"));
+
+        assert_eq!(Lang::AR, Lang::try_from(String::from("ar"))?);
+        assert_eq!(Lang::EN, Lang::try_from(String::from("en"))?);
+        assert_eq!(Lang::GR, Lang::try_from(String::from("gr"))?);
+        assert_eq!(Lang::RO, Lang::try_from(String::from("ro"))?);
+
+        assert!(Lang::try_from(String::from("unknown")).is_err());
+
+        Ok(())
+    }
+}
