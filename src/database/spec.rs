3c5729e342d6376f1ab2115c602945dbff6bc15f
Alexandru N. Onea <onea.alex@gmail.com>
2023-08-01 09:44:20 +0300

    wip: database: load spec

---
+++ src/database/spec.rs
@@ -0,0 +1,38 @@
+use std::path::PathBuf;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
+#[derive(Deserialize, Serialize)]
+pub struct DatabaseSection {
+	collections: Vec<String>,
+}
+
+#[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
+#[derive(Deserialize, Serialize)]
+pub struct DatabaseSpec {
+	#[serde(skip)]
+	root: PathBuf,
+	database: DatabaseSection,
+}
+
+impl TryFrom<PathBuf> for DatabaseSpec {
+	type Error = crate::error::Error;
+	fn try_from(value: PathBuf) -> Result<Self, Self::Error> {
+		let content = std::fs::read_to_string(&value)?;
+		let mut spec: DatabaseSpec = toml::from_str(&content)?;
+
+		spec.root = value.parent().unwrap().to_path_buf();
+		Ok(spec)
+	}
+}
+
+impl DatabaseSpec {
+	pub fn root(&self) -> &PathBuf {
+		&self.root
+	}
+
+	pub fn collections(&self) -> &Vec<String> {
+		&self.database.collections
+	}
+}
