51005e265ad5837c710929861d0fd9a675d171e3
Alexandru N. Onea <onea.alex@gmail.com>
2023-09-10 12:39:44 +0300

    serde: enforce collection/id format for references

--- lib/src/serde/reference.rs
+++ lib/src/serde/reference.rs
@@ -1,46 +1,45 @@
 use super::{property::Property, Error, Result};
+use regex::Regex;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(try_from = "String")]
 #[serde(into = "String")]
 pub struct ReferenceProperty {
-    collection: Option<String>,
+    collection: String,
     id: String,
 }
 
 impl TryFrom<String> for ReferenceProperty {
     type Error = Error;
     fn try_from(value: String) -> Result<Self> {
-        value
-            .strip_prefix("@")
-            .ok_or(Error::InvalidReference(value.to_owned()))
-            .and_then(|res| {
-                Ok(match res.split_once("/") {
-                    Some((collection, id)) => ReferenceProperty {
-                        collection: Some(collection.to_owned()),
-                        id: id.to_owned(),
-                    },
-                    None => ReferenceProperty {
-                        collection: None,
-                        id: res.to_owned(),
-                    },
+        // We unwrap with confidence since the regex is correct and below the
+        // size limit.
+        let re = Regex::new(r"\A@(\w+)/([\w-]+)\z").unwrap();
+        let cap = re.captures(&value);
+
+        match cap {
+            Some(capture) => {
+                let (_, [collection, id]) = capture.extract();
+
+                Ok(ReferenceProperty {
+                    collection: collection.to_owned(),
+                    id: id.to_owned(),
                 })
-            })
+            }
+            None => Err(Error::InvalidReference(value)),
+        }
     }
 }
 
 impl From<ReferenceProperty> for String {
     fn from(value: ReferenceProperty) -> Self {
-        match value.collection {
-            Some(collection) => format!("@{}/{}", collection, value.id),
-            None => format!("@{}", value.id),
-        }
+        format!("@{}/{}", value.collection, value.id)
     }
 }
 
-impl Property<(Option<String>, String)> for ReferenceProperty {
-    fn value(&self, _: Option<super::Lang>) -> Option<(Option<String>, String)> {
+impl Property<(String, String)> for ReferenceProperty {
+    fn value(&self, _: Option<super::Lang>) -> Option<(String, String)> {
         Some((self.collection.to_owned(), self.id.to_owned()))
     }
 }
