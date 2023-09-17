ff04781f405bb47ec6b06c7b9dfea1144e7e0985
Alexandru N. Onea <onea.alex@gmail.com>
2023-09-17 21:06:01 +0300

    cli: dump demo with param

--- cli/src/dump.rs
+++ cli/src/dump.rs
@@ -3,33 +3,20 @@ use argh::FromArgs;
 use cufarul::{
     db::Database,
     error::Result,
-    model::{CollectionKey, Composition, Model, Person, Publication, Taxonomy, Text},
-    repo::{CufarulRepository, Repository, RepositorySpec},
+    model::{CollectionKey, Composition, Lang, Model, Person, Publication, Taxonomy, Text},
+    repo::{Cufarul, CufarulRepository, Repository, RepositorySpec},
 };
 
 /// dump contents of the database
 #[derive(Clone, Debug, FromArgs)]
 #[argh(subcommand, name = "dump")]
-pub struct DumpConfig {}
+pub struct DumpConfig {
+    /// id to dump
+    #[argh(positional)]
+    id: Option<String>,
+}
 
-pub fn dump(args: Args, _config: DumpConfig) -> Result<()> {
-    let mut repo = RepositorySpec::from_path(
-        args.repo
-            .unwrap_or(std::env::current_dir().unwrap_or_default())
-            .as_path(),
-    )
-    .map_err(|err| err.into())
-    .and_then(|spec| CufarulRepository::from_spec(spec))?;
-
-    println!(
-        "Found repository version {} at {:?}",
-        repo.spec().version(),
-        repo.spec().root()
-    );
-
-    println!("Syncing...");
-    repo.sync()?;
-
+fn dump_all(repo: CufarulRepository) -> Result<()> {
     for (id, node) in repo.db().nodes_iter() {
         let data = node.data();
         let object: &dyn Model = match id {
@@ -86,4 +73,34 @@ pub fn dump(args: Args, _config: DumpConfig) -> Result<()> {
     // }
 
     Ok(())
+}
+
+pub fn dump(args: Args, config: DumpConfig) -> Result<()> {
+    let mut repo = RepositorySpec::from_path(
+        args.repo
+            .unwrap_or(std::env::current_dir().unwrap_or_default())
+            .as_path(),
+    )
+    .map_err(|err| err.into())
+    .and_then(|spec| CufarulRepository::from_spec(spec))?;
+
+    println!(
+        "Found repository version {} at {:?}",
+        repo.spec().version(),
+        repo.spec().root()
+    );
+
+    println!("Syncing...");
+    repo.sync()?;
+
+    match config.id {
+        Some(id) => {
+            let node = repo.model_by_id(CollectionKey::Composition(id.into()), Some(Lang::RO))?;
+            let repr = serde_json::to_string_pretty(&node).expect("ofof");
+
+            println!("{repr}");
+            Ok(())
+        }
+        None => dump_all(repo),
+    }
 }
